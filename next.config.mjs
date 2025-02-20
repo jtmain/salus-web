28ddc74451353a01e65b9a33b4c82d585b65f81c
Yash Trivedi <36705114+YT-Main@users.noreply.github.com>
2025-02-20 04:30:07 -0500

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      formats: ['image/avif', 'image/webp', 'image/png', 'image/jpeg'], // Allow both PNG & JPG
       unoptimized: true, // Bypass Next.js image optimization
     },
   };
